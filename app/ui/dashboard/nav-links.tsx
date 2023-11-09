146f124323a379a2537c0bdf951b01ab6f309d54
kamolchai <golf.kamolchai@gmail.com>
2023-11-10 00:06:02 +0700

    test2

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -34,8 +34,7 @@ export default function NavLinks() {
               {
                 'bg-sky-100 text-blue-600': pathname === link.href,
               },
-            )}
-            >
+            )}>
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
           </a>
