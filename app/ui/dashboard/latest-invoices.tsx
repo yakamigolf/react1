c5bb49e28d8c3cfdcf636267a502c53bdfb25072
kamolchai <golf.kamolchai@gmail.com>
2023-11-14 23:20:39 +0700

    add invoice table

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -4,11 +4,12 @@ import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
 import { LatestInvoice } from '@/app/lib/definitions';
 import { formatDateToLocal } from '@/app/lib/utils';
-export default async function LatestInvoices({
-  latestInvoices,
-}: {
-  latestInvoices: LatestInvoice[];
-}) {
+
+import { fetchLatestInvoices } from '@/app/lib/data';
+
+
+export default async function LatestInvoices(){
+  const latestInvoices = (await fetchLatestInvoices()).props.latestInvoices;
   return (
     <div className="flex w-full flex-col md:col-span-4 lg:col0-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
