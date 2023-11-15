31b9c701dffdd751266baf544d091ed7ba4028cd
kamolchai <golf.kamolchai@gmail.com>
2023-11-15 21:35:55 +0700

    add retry button

--- app/dashboard/invoices/errror.tsx
+++ app/dashboard/invoices/errror.tsx
@@ -16,7 +16,7 @@ export default function Error({
  
   return (
     <main className="flex h-full flex-col items-center justify-center">
-      <h2 className="text-center">ใจเย็นไอ้สอง เออเล่อเสียแล้ว เห้อ...</h2>
+      <h2 className="text-center">ใจเย็นไอ้สอง เออเล่อเสียแล้ว เห้อ...ลองใหม่มะ</h2>
       <button
         className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
         onClick={
@@ -24,7 +24,7 @@ export default function Error({
           () => reset()
         }
       >
-        Try again
+        ลองกดดู
       </button>
     </main>
   );
